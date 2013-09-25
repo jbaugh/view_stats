65d8613a6e9696ab285516026de10e92a72157f8
Jarrett Baugh <jarrett.baugh@gmail.com>
2013-09-24 21:51:44 -0700

    Get html rendering working

--- lib/view_stats/render_tree.rb
+++ lib/view_stats/render_tree.rb
@@ -13,12 +13,15 @@ module ViewStats
 
     def self.visualize(view)
       @@parsed_views = []
+      @@found_base_file = false
 
       results = self.find_and_parse_view(view)
       if !results.empty?
         results = [:path => view, :renders => results]
-        self.display_text_tree(results)
-        true
+        self.display_output(results)
+        return true
+      elsif @@found_base_file
+        puts "#{view} does not render any other views"
       else
         puts "Could not find a view for #{view}"
       end
@@ -35,7 +38,33 @@ module ViewStats
         puts parsed_view_path if parsed_view_path
       end
 
-      false
+      return false
+    end
+
+    private
+
+    def self.display_output(results)
+      display_text_tree(results)
+    end
+
+    def self.display_html(results)
+      html_str = ""
+      html_str << "<html><body>"
+      html_str << display_html_inner(results)
+      html_str << "</body></html>"
+
+      puts html_str
+    end
+
+    def self.display_html_inner(results, depth = 0)
+      html_str = ""
+      html_str << "<ul>"
+
+      results.each do |res|
+        html_str << "<li>#{res[:path]}</li> " 
+        html_str << self.display_html_inner(res[:renders], depth + 1)
+      end
+      html_str << "</ul>"
     end
 
     def self.display_text_tree(results, depth = 0)
@@ -59,6 +88,7 @@ module ViewStats
 
     def self.parse_view(view_path)
       return [] unless File.exists?(view_path)
+      @@found_base_file = true
       @@parsed_views << view_path
 
       view_data = File.read(view_path)
